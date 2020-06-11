7283f6e6fbf633f3a77b7a439455d9a6bbd40186
kko0831 <kko0831@hanmail.net>
2020-06-11 17:23:10 +0900

    #2.4 Apollo Setup Recap

--- src/apollo.ts
+++ src/apollo.ts
@@ -8,6 +8,36 @@ const apolloClient = new ApolloClient({
         isLoggedIn: Boolean(localStorage.getItem("jwt")),
       },
     },
+    resolvers: {
+      Mutation: {
+        // 인자는 순서대로, parent, args, contexg 다. 서버와 동일하다.
+        logUserIn: (_, { token }, { cache }) => {
+          localStorage.setItem("jwt", token);
+          cache.writeData({
+            data: {
+              auth: {
+                __typename: "Auth",
+                isLoggedIn: true,
+              },
+            },
+          });
+          return null;
+        },
+        // 인자는 순서대로, parent, args, contexg 다. 서버와 동일하다.
+        logUserOut: (_, __, { cache }) => {
+          localStorage.removeItem("jwt");
+          cache.writeData({
+            data: {
+              auth: {
+                __typename: "Auth",
+                isLoggedIn: false,
+              },
+            },
+          });
+          return null;
+        },
+      },
+    },
   },
   request: async (operation: Operation) => {
     operation.setContext({
