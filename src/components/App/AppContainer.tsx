c9be1f0d5318c0ad3076ed13da0719b179d6b632
kko0831 <kko0831@hanmail.net>
2020-06-13 01:41:15 +0900

    #2.17 Notifications with React Toastify

--- src/components/App/AppContainer.tsx
+++ src/components/App/AppContainer.tsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { graphql } from "react-apollo";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
 import GlobalStyle from "../../global-styles";
 import { theme } from "../../theme";
 import { ThemeProvider } from "../../typed-components";
@@ -7,12 +9,13 @@ import AppPresenter from "./AppPresenter";
 import { IS_LOGGED_IN } from "./AppQueries";
 
 const AppContainer: any = ({ data }) => (
-  <>
+  <Fragment>
     <GlobalStyle />
     <ThemeProvider theme={theme}>
       <AppPresenter isLoggedIn={data.auth.isLoggedIn} />
     </ThemeProvider>
-  </>
+    <ToastContainer draggable={true} position="bottom-center" />
+  </Fragment>
 );
 
 export default graphql(IS_LOGGED_IN)(AppContainer);
