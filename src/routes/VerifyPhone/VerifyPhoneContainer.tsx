4e7ab263def19c9dc56fb7065807dd4698508b8b
kko0831 <kko0831@hanmail.net>
2020-06-21 00:30:55 +0900

    #2.25 VerifyPhone Mutation part Two

--- src/routes/VerifyPhone/VerifyPhoneContainer.tsx
+++ src/routes/VerifyPhone/VerifyPhoneContainer.tsx
@@ -1,35 +1,66 @@
 import React from "react";
+import { Mutation } from "react-apollo";
 import { RouteComponentProps } from "react-router-dom";
+import { toast } from "react-toastify";
+import { verfiyPhone, verfiyPhoneVariables } from "../../types/api";
+import { VERIFY_PHONE } from "./VerifyPhone.queries";
 import VerifyPhonePresenter from "./VerifyPhonePresenter";
 
 interface IState {
   verificationCode: string;
+  phoneNumber: string;
 }
 
-interface IProps extends RouteComponentProps<any> {}
+interface IProps
+  extends RouteComponentProps<{ phone?: string }, any, { phone?: string }> {}
+
+class VerifyMutation extends Mutation<verfiyPhone, verfiyPhoneVariables> {}
 
 class VerifyPhoneContainer extends React.Component<IProps, IState> {
   constructor(props: IProps) {
     super(props);
     // tslint:disable-next-line
-    console.log(props);
+    console.log(props.location.state.phone);
     try {
       Object.hasOwnProperty.call(props.location.state, "phone");
     } catch (e) {
       props.history.push("/");
     }
     this.state = {
+      phoneNumber: props.location.state.phone,
       verificationCode: "",
     };
     this.onInputChange = this.onInputChange.bind(this);
   }
   public render() {
-    const { verificationCode } = this.state;
+    const { verificationCode, phoneNumber } = this.state;
     return (
-      <VerifyPhonePresenter
-        onChange={this.onInputChange}
-        verificationCode={verificationCode}
-      />
+      <VerifyMutation
+        mutation={VERIFY_PHONE}
+        variables={{
+          key: verificationCode,
+          phoneNumber,
+        }}
+        onCompleted={(data) => {
+          const { CompletePhoneVerification } = data;
+          if (CompletePhoneVerification.ok) {
+            toast.success("You're verified, loggin in now");
+            // tslint:disable-next-line
+            console.log(CompletePhoneVerification);
+          } else {
+            toast.error(CompletePhoneVerification.error);
+          }
+        }}
+      >
+        {(mutation, { loading }) => (
+          <VerifyPhonePresenter
+            onSubmit={mutation}
+            onChange={this.onInputChange}
+            verificationCode={verificationCode}
+            loading={loading}
+          />
+        )}
+      </VerifyMutation>
     );
   }
 
