baf5bea80f9bc29cbe57eea3366ba8d512d8948c
kko0831 <kko0831@hanmail.net>
2020-06-26 03:13:32 +0900

    #2.41 Settings Screen part One + #2.42 Settings Screen part Two

---
+++ src/routes/Settings/SettingsContainer.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Mutation, Query } from "react-apollo";
+import { LOG_USER_OUT } from "../../innerQueries";
+import { GET_PLACES, USER_PROFILE } from "../../sharedQueries.queries";
+import { getPlaces, userProfile } from "../../types/api";
+import SettingsPresenter from "./SettingsPresenter";
+
+class MiniProfileQuery extends Query<userProfile> {}
+class PlacesQuery extends Query<getPlaces> {}
+
+class SettingsContainer extends React.Component {
+  public render() {
+    return (
+      <Mutation mutation={LOG_USER_OUT}>
+        {(logUserOut) => (
+          <MiniProfileQuery query={USER_PROFILE}>
+            {({ data, loading: userDataLoading }) => (
+              <PlacesQuery query={GET_PLACES}>
+                {({ data: placesData, loading: placesLoading }) => (
+                  <SettingsPresenter
+                    userDataLoading={userDataLoading}
+                    userData={data}
+                    placesLoading={placesLoading}
+                    placesData={placesData}
+                    logUserOut={logUserOut}
+                  />
+                )}
+              </PlacesQuery>
+            )}
+          </MiniProfileQuery>
+        )}
+      </Mutation>
+    );
+  }
+}
+
+export default SettingsContainer;
---
+++ src/routes/Settings/SettingsPresenter.tsx
@@ -0,0 +1,113 @@
+import Header from "components/Header";
+import Place from "components/Place";
+import React from "react";
+import { MutationFn } from "react-apollo";
+import Helmet from "react-helmet";
+import { Link } from "react-router-dom";
+import styled from "../../typed-components";
+import { getPlaces, userProfile } from "../../types/api";
+
+const Container = styled.div`
+  padding: 0px 40px;
+`;
+
+const Image = styled.img`
+  height: 60px;
+  width: 60px;
+  border-radius: 50%;
+`;
+
+const GridLink = styled(Link)`
+  display: grid;
+  grid-template-columns: 1fr 4fr;
+  grid-gap: 10px;
+  margin-bottom: 10px;
+`;
+
+const Keys = styled.div``;
+
+const Key = styled.span`
+  display: block;
+  cursor: pointer;
+`;
+
+const FakeLink = styled.span`
+  text-decoration: underline;
+  cursor: pointer;
+`;
+
+const SLink = styled(Link)`
+  display: block;
+  text-decoration: underline;
+  margin: 20px 0px;
+`;
+
+interface IProps {
+  logUserOut: MutationFn;
+  userData?: userProfile;
+  placesData?: getPlaces;
+  userDataLoading: boolean;
+  placesLoading: boolean;
+}
+
+const SettingsPresenter: React.SFC<IProps> = ({
+  logUserOut,
+  userData,
+  placesData,
+  userDataLoading,
+  placesLoading,
+}) => {
+  const GetMyProfile = userData;
+  const GetMyPlaces = placesData;
+  if (
+    GetMyProfile &&
+    GetMyProfile.GetMyProfile &&
+    GetMyProfile.GetMyProfile.ok
+  ) {
+    const user = GetMyProfile.GetMyProfile.user;
+    return (
+      <React.Fragment>
+        <Helmet>
+          <title>Settings | Nuber</title>
+        </Helmet>
+        <Header title={"Account Settings"} backTo={"/"} />
+        <Container>
+          <GridLink to={"/edit-account"}>
+            {!userDataLoading &&
+              user &&
+              user.profilePhoto &&
+              user.email &&
+              user.fullName && (
+                <React.Fragment>
+                  <Image src={user.profilePhoto} />
+                  <Keys>
+                    <Key>{user.fullName}</Key>
+                    <Key>{user.email}</Key>
+                  </Keys>
+                </React.Fragment>
+              )}
+          </GridLink>
+          {!placesLoading &&
+            GetMyPlaces &&
+            GetMyPlaces.GetMyPlaces &&
+            GetMyPlaces.GetMyPlaces.ok &&
+            GetMyPlaces.GetMyPlaces.places &&
+            GetMyPlaces.GetMyPlaces.places.map((place) => (
+              <Place
+                key={place!.id}
+                id={place!.id}
+                fav={place!.isFav}
+                name={place!.name}
+                address={place!.address}
+              />
+            ))}
+          <SLink to={"/places"}>Go to Places</SLink>
+          <FakeLink onClick={logUserOut as any}>Log Out</FakeLink>
+        </Container>
+      </React.Fragment>
+    );
+  } else {
+    return <div>can't load</div>;
+  }
+};
+export default SettingsPresenter;
