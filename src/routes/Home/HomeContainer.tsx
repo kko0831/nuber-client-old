d8fb001f2fb79d7b6edd54b20509bf5f97a93f5f
kko0831 <kko0831@hanmail.net>
2020-07-08 17:36:05 +0900

    #2.54 HomeScreen User Marker

--- src/routes/Home/HomeContainer.tsx
+++ src/routes/Home/HomeContainer.tsx
@@ -1,21 +1,46 @@
 import React from "react";
 import { Query } from "react-apollo";
+import ReactDOM from "react-dom";
 import { RouteComponentProps } from "react-router";
 import { USER_PROFILE } from "../../sharedQueries.queries";
 import { userProfile } from "../../types/api";
 import HomePresenter from "./HomePresenter";
 
-interface IProps extends RouteComponentProps<any> {}
+interface IProps extends RouteComponentProps<any> {
+  google: any;
+}
+
 interface IState {
   isMenuOpen: boolean;
+  lat: number;
+  lng: number;
 }
 
 class ProfileQuery extends Query<userProfile> {}
 
 class HomeContainer extends React.Component<IProps, IState> {
+  public mapRef: any;
+  public map: google.maps.Map | null = null;
+  public userMarker: google.maps.Marker | null = null;
+
   public state = {
     isMenuOpen: false,
+    lat: 0,
+    lng: 0,
   };
+
+  constructor(props) {
+    super(props);
+    this.mapRef = React.createRef();
+  }
+
+  public componentDidMount() {
+    navigator.geolocation.watchPosition(
+      this.handleGeoSuccess,
+      this.handleGeoError
+    );
+  }
+
   public render() {
     const { isMenuOpen } = this.state;
     return (
@@ -25,6 +50,7 @@ class HomeContainer extends React.Component<IProps, IState> {
             loading={loading}
             isMenuOpen={isMenuOpen}
             toggleMenu={this.toggleMenu}
+            mapRef={this.mapRef}
           />
         )}
       </ProfileQuery>
@@ -36,6 +62,68 @@ class HomeContainer extends React.Component<IProps, IState> {
         isMenuOpen: !state.isMenuOpen,
       };
     });
+  };
+
+  public handleGeoSuccess: PositionCallback = (position: Position) => {
+    const {
+      coords: { latitude, longitude },
+    } = position;
+    this.setState({
+      lat: latitude,
+      lng: longitude,
+    });
+    this.loadMap(latitude, longitude);
+  };
+
+  public handleGeoError: PositionErrorCallback = () => {
+    // tslint:disable-next-line
+    console.error("No location");
+  };
+
+  public loadMap = (lat, lng) => {
+    const { google } = this.props;
+    const maps = google.maps;
+    const mapNode = ReactDOM.findDOMNode(this.mapRef.current);
+    const mapConfig: google.maps.MapOptions = {
+      center: {
+        lat,
+        lng,
+      },
+      disableDefaultUI: true,
+      zoom: 11,
+    };
+    this.map = new maps.Map(mapNode, mapConfig);
+    const watchOptions: PositionOptions = {
+      enableHighAccuracy: true,
+    };
+    navigator.geolocation.watchPosition(
+      this.handleGeoWatchSuccess,
+      this.handleGeoError,
+      watchOptions
+    );
+    const userMarkerOption: google.maps.MarkerOptions = {
+      icon: {
+        path: maps.SymbolPath.CIRCLE,
+        scale: 7,
+      },
+      position: {
+        lat,
+        lng,
+      },
+    };
+    this.userMarker = new maps.Marker(userMarkerOption);
+    this.userMarker!.setMap(this.map);
+  };
+
+  public handleGeoWatchSuccess: PositionCallback = (position: Position) => {
+    // tslint:disable-next-line
+    console.log(position);
+    return;
+  };
+
+  public handleGeoWatchError: PositionErrorCallback = () => {
+    // tslint:disable-next-line
+    console.error("No location");
   };
 }
 
