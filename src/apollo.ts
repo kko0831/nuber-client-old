0c4d73f58a186279b8ad2e07f4812f1d93952980
kko0831 <kko0831@hanmail.net>
2020-07-16 18:33:56 +0900

    #2.66 Ejecting from Apollo Boost

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,52 +1,109 @@
-import ApolloClient, { Operation } from "apollo-boost";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloClient } from "apollo-client";
+import { ApolloLink, concat, Operation, split } from "apollo-link";
+import { onError } from "apollo-link-error";
+import { HttpLink } from "apollo-link-http";
+import { withClientState } from "apollo-link-state";
+import { WebSocketLink } from "apollo-link-ws";
+import { getMainDefinition } from "apollo-utilities";
+import { toast } from "react-toastify";
+
+const getToken = () => {
+  const token = localStorage.getItem("jwt");
+  return token || "";
+};
+
+const cache = new InMemoryCache();
+
+const authMiddleware = new ApolloLink((operation: Operation, forward: any) => {
+  operation.setContext({
+    headers: {
+      "X-JWT": getToken(),
+    },
+  });
+  return forward(operation);
+});
+
+const httpLink = new HttpLink({
+  uri: "http://localhost:4000/graphql",
+});
+
+const wsLink = new WebSocketLink({
+  options: {
+    connectionParams: {
+      "X-JWT": getToken(),
+    },
+    reconnect: true,
+  },
+  uri: "ws://localhost:4000/subscription",
+});
+
+const combinedLinks = split(
+  ({ query }) => {
+    const { kind, operation }: any = getMainDefinition(query);
+    return kind === "OperationDefinition" && operation === "subscription";
+  },
+  wsLink,
+  httpLink
+);
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) {
+    // eslint-disable-next-line array-callback-return
+    graphQLErrors.map(({ message }) => {
+      toast.error(`Unexpected error: ${message}`);
+    });
+  }
+  if (networkError) {
+    toast.error(`Network error: ${networkError}`);
+  }
+});
+
+const localStateLink = withClientState({
+  cache,
+  defaults: {
+    auth: {
+      __typename: "Auth",
+      isLoggedIn: Boolean(localStorage.getItem("jwt")),
+    },
+  },
+  resolvers: {
+    Mutation: {
+      logUserIn: (_, { token }, { cache: appCache }) => {
+        localStorage.setItem("jwt", token);
+        cache.writeData({
+          data: {
+            auth: {
+              __typename: "Auth",
+              isLoggedIn: true,
+            },
+          },
+        });
+        return null;
+      },
+      logUserOut: (_, __, { cache: appCache }) => {
+        localStorage.removeItem("jwt");
+        cache.writeData({
+          data: {
+            auth: {
+              __typename: "Auth",
+              isLoggedIn: false,
+            },
+          },
+        });
+        return null;
+      },
+    },
+  },
+});
 
 const apolloClient = new ApolloClient({
-  clientState: {
-    defaults: {
-      auth: {
-        __typename: "Auth",
-        isLoggedIn: Boolean(localStorage.getItem("jwt")),
-      },
-    },
-    resolvers: {
-      Mutation: {
-        // 인자는 순서대로, parent, args, contexg 다. 서버와 동일하다.
-        logUserIn: (_, { token }, { cache }) => {
-          localStorage.setItem("jwt", token);
-          cache.writeData({
-            data: {
-              auth: {
-                __typename: "Auth",
-                isLoggedIn: true,
-              },
-            },
-          });
-          return null;
-        },
-        // 인자는 순서대로, parent, args, contexg 다. 서버와 동일하다.
-        logUserOut: (_, __, { cache }) => {
-          localStorage.removeItem("jwt");
-          cache.writeData({
-            data: {
-              auth: {
-                __typename: "Auth",
-                isLoggedIn: false,
-              },
-            },
-          });
-          return null;
-        },
-      },
-    },
-  },
-  request: async (operation: Operation) => {
-    operation.setContext({
-      headers: {
-        "X-JWT": localStorage.getItem("jwt"),
-      },
-    });
-  },
-  uri: "http://localhost:4000/graphql",
+  cache,
+  link: ApolloLink.from([
+    errorLink,
+    localStateLink,
+    concat(authMiddleware, combinedLinks),
+  ]),
 });
 
 export default apolloClient;
